9d694ed0902352727cfabffece20bfca577c4335
hello-mr-vishu <vishnurongali21@gmail.com>
2025-07-27 23:24:39 +0530

    extracted data from pdf files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from os import name
+from setuptools import find_packages , setup
+
+setup(
+    name="rag-kit",
+    version="0.1.0",
+    author="Vicky",
+    author_email="vishnurongali21@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.10"
+)
